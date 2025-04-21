07b1ca8b4d25f0ad2bb1a253c8d11ddbbdfd6aae
kaabilcoder <kaabilcoder@gmail.com>
2025-04-21 19:17:43 +0530

    minor updates on db

--- src/models/db.ts
+++ src/models/db.ts
@@ -2,11 +2,9 @@ import mongoose, {Schema, model} from "mongoose"
 import dotenv from 'dotenv'
 
 dotenv.config();
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017"
-const DB_NAME = process.env.DB_NAME || "urlShortner";
 
-mongoose.connect(`${MONGODB_URI}/${DB_NAME}`)
-    .then(() => console.log("Connected to MongoDB"))
+mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`)
+    .then(() => console.log(`Connected to MongoDB at ${mongoose.connection.name}`))
     .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 const UrlSchema = new Schema({
