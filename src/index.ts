1800fa2a85e3e22e408782c68c525ff9f4f482c7
kaabilcoder <kaabilcoder@gmail.com>
2025-04-21 13:27:59 +0530

    frontend is done using ejs, working frontend and backend

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import express from "express";
 import { UrlModel } from "./models/db";
 import { router as urlRoute } from './routes/url';
+import path from "path";
 import { handleRedirectUrl } from "./controllers/handleRedirectUrl";
+
 const app = express();
 const PORT = 8000;
+
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "src", "views"));
 
 app.use('/url', urlRoute)
-app.get('/:shortId', handleRedirectUrl)
+app.get('/url/:shortId', handleRedirectUrl)
 
+app.get("/home", async (req, res) => {
+    const allUrl = await UrlModel.find({});
+    return res.render('home',{
+        urls: allUrl
+    })
+})
 
 app.listen(PORT, () => console.log(`server is running on the port ${PORT}`))
